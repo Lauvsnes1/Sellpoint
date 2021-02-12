949eac1ec4bb1ca18e2856d30e05035414cceb18
Even Lauvsnes <evenlau@stud.ntnu.no>
2021-02-12 14:21:10 +0100

    Fått på plass app bar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import AppBar from '../components/header'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <AppBar>
+
+      </AppBar>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
