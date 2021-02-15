493e7bb0487fba706a30b22d24efd8b48683a38e
Andrea Hetlevik Vanebo <vanboand@gmail.com>
2021-02-15 11:10:36 +0100

    pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,33 @@ import Link from "next/link"
 import Image from "next/image"
 import Layout from '../components/layout'
 import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Test() {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+      props: {
+        allPostsData
+      }
+    }
+  }
+
+export default function Test({ allPostsData }) {
     return (
         <>
         <Layout>
         <Head>
             <title>Sellpoint - Forside</title>
         </Head>
-        <h2 className={utilStyles.headingMd}>
+        <div className={utilStyles.heading2Xl}>
+        <h2>
             <Link href="/Annonse">
                 <a>
                 Spisebord
                 </a>
             </Link>
-        </h2>,
+        </h2>
+        </div>,
         <Image src="/images/spisebord.jpg" height={214} width={320} alt="Spisebord"/>
         <h3 className="link">
             <Link href="/Annonse">
