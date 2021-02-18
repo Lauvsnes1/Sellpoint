3b7f97985a4d601e7fb7c8c0ad064d41997cafcd
Andrea Hetlevik Vanebo <vanboand@gmail.com>
2021-02-18 17:31:44 +0100

    oppdatering på merge #9

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-<<<<<<< HEAD
 import AppBar from '../components/header'
 import Card from '../components/cards'
 import PostCards from '../components/cards_alt';
 import fire from '../config/fire-config'
-=======
-import fire from '../config/fire-config'
-import AppBar from '../components/header'
->>>>>>> e41883ea04d4dc44219a7de48d3b04a433dda1d3
 
 export default function Home() {
   return (
