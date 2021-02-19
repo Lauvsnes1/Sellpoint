d2910a6f042c003c9de9d0c8a7cacf2e8e0ceb1f
Even Lauvsnes <evenlau@stud.ntnu.no>
2021-02-19 17:48:15 +0100

    lagt til logo på headern

--- components/header.js
+++ components/header.js
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { colors, ThemeProvider} from '@material-ui/core';
 import { green, blue, yellow, darkGreen, lightGreen }from '@material-ui/core/colors/green'
 import theme from '../src/theme';
+import Image from 'next/image'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -41,9 +42,7 @@ const useStyles = makeStyles((theme) => ({
             <div className={classes.root}>
             <AppBar color='primary' variant= 'elevation'>
                 <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                    Logo(bonde mekker)
-                </Typography>
+                  <Image src='/Logo.png' height={75} width={150} />
                 <Button className = {classes.button}> Hjem</Button>
                 <Button className = {classes.button}> Mine annonser</Button>
                 <Button className = {classes.button}> Mine favoritter</Button>
