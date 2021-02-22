c66a259ac2bd6dc011657b56bf9c254db0ccd32e
njal3001 <73302347+njal3001@users.noreply.github.com>
2021-02-22 15:21:16 +0100

    Fikset styling (#9)

--- pages/createPost.js
+++ pages/createPost.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import fire from "../config/fire-config";
+import AppBar from "../components/header";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import { useRouter } from "next/router";
 import uniqid from "uniqid";
 
@@ -75,7 +79,7 @@ const CreatePost = () => {
       });
 
     // Create post
-    var document = fire.firestore().collection("posts").add({
+    var document = await fire.firestore().collection("posts").add({
       title: title,
       place: location,
       price: price,
@@ -85,8 +89,7 @@ const CreatePost = () => {
       imageUrl: downloadUrl,
       imageRef: ref,
     });
-
-    router.push("/");
+    router.push("/annonse/" + document.id);
     //Det tar litt tid før den fullfører så burde ha noe som viser at den laster ellerno
   };
 
@@ -97,58 +100,118 @@ const CreatePost = () => {
 
   return (
     <div>
-      <h1>Opprett en ny annonse:</h1>
-      {notification}
-      <form onSubmit={handleSubmit}>
-        Overskrift på annonsen:{" "}
-        <input
-          type="text"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-        <br />
-        Plassering:{" "}
-        <input
-          type="text"
-          value={location}
-          onChange={({ target }) => setLocation(target.value)}
-        />
-        <br />
-        Pris:{" "}
-        <input
-          type="number"
-          min="0"
-          value={price}
-          onChange={({ target }) => setPrice(target.value)}
-        />
-        <br />
-        Ingress:{" "}
-        <textarea
-          value={miniDescription}
-          onChange={({ target }) => setMiniDescription(target.value)}
-        />
-        <br />
-        Beskrivelse av varen:{" "}
-        <textarea
-          value={description}
-          onChange={({ target }) => setDescription(target.value)}
-        />
-        <br />
-        Last opp bilde av varen:{" "}
-        <input
-          type="file"
-          accept="image/*"
-          multiple={false}
-          onChange={handleImageUpload}
-        />
-        <br />
-        {/*Burde kanskje lage en egen ImageUpload component eller ImageDisplay component
-        siden det kan være flere steder der man vil laste opp bilder?*/}
-        {/*Skal bildet som lastes opp vises på denne siden?*/}
-        <img src={imageSrc} />
-        <br />
-        <button type="submit">Opprett</button>
-      </form>
+      <style jsx>{`
+        .container {
+          width: 80%;
+          max-width: 700px;
+          margin: auto;
+          margin-top: 5rem;
+        }
+        h1 {
+          font-family: "helvetica neue";
+          font-size: 48pt;
+          font-weight: normal;
+        }
+        span {
+          color: #90cc00;
+          font-weight: bold;
+        }
+        a {
+          text-decoration: underline;
+        }
+        .buttons {
+          display: flex;
+          flex-direction: column;
+        }
+        .textfield {
+          margin: 20px 0;
+        }
+      `}</style>
+      <AppBar />
+      <div className="container">
+        {notification}
+        {imageSrc && <Image src={imageSrc} width={600} height={400} />}
+        <div className="buttons">
+          <Button
+            style={{ width: "200px" }}
+            color="secondary"
+            variant="contained"
+            component="label"
+          >
+            Last opp bilde av varen
+            <input
+              type="file"
+              accept="image/*"
+              multiple={false}
+              onChange={handleImageUpload}
+              hidden
+            />
+          </Button>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="textfield">
+            <TextField
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+              id="outlined-title"
+              label="Tittel"
+              variant="outlined"
+              fullWidth
+            />
+          </div>
+          <div className="textfield">
+            <TextField
+              value={location}
+              onChange={({ target }) => setLocation(target.value)}
+              id="outlined-location"
+              label="Lokasjon"
+              variant="outlined"
+            />
+          </div>
+          <div className="textfield">
+            <TextField
+              value={price}
+              onChange={({ target }) => setPrice(target.value)}
+              id="outlined-price"
+              label="Pris"
+              variant="outlined"
+            />
+          </div>
+          <div className="textfield">
+            <TextField
+              value={description}
+              onChange={({ target }) => setDescription(target.value)}
+              id="outlined-description"
+              label="Beskrivelse"
+              variant="outlined"
+              multiline
+              fullWidth
+            />
+          </div>
+          <div className="textfield">
+            <TextField
+              value={miniDescription}
+              onChange={({ target }) => setMiniDescription(target.value)}
+              id="outlined-ingress"
+              label="Ingress"
+              variant="outlined"
+              multiline
+              fullWidth
+            />
+          </div>
+          <div className="buttons">
+            <Button
+              style={{ width: "200px" }}
+              color="secondary"
+              variant="contained"
+              //type="submit"
+              onClick={handleSubmit}
+            >
+              Opprett annonse
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
