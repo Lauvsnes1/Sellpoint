22faf917da997e571ea1c352d5f723e021069f84
Lavrans Kaul Sagen <lavrans.ks@gmail.com>
2021-02-24 13:03:33 +0100

    Lagt til admin-melding for admins

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import AppBar from '../components/header'
-import Card from '../components/cards'
-import PostCards from '../components/cards_alt';
-import fire from '../config/fire-config'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import AppBar from "../components/header";
+import Card from "../components/cards";
+import PostCards from "../components/cards_alt";
+import fire from "../config/fire-config";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
+  const [admin, setAdmin] = useState(false);
+  useEffect(() => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        fire
+          .firestore()
+          .collection("users")
+          .doc(user.uid)
+          .get()
+          .then((doc) => setAdmin(doc.data()?.permissions.admin ? true : false));
+      }
+    });
+  });
 
-    <div className={styles.container}> 
-      <AppBar/>
+  return (
+    <div className={styles.container}>
+      <AppBar />
       <div className={styles.rad}>
         <div className={styles.annonseContainer}>
-            <PostCards/>
+          <AdminHeadLine admin={admin} />
+          <PostCards />
         </div>
       </div>
-     </div>
-  )
+    </div>
+  );
+}
+
+function AdminHeadLine(props) {
+  const admin = props.admin;
+
+  if (admin) {
+    return <h1>Du er logget inn som admin</h1>;
+  } else {
+    return <></>;
+  }
 }
