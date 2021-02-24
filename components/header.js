80538465322f1a0b19042d994e6997acf1498411
Lavrans Kaul Sagen <lavrans.ks@gmail.com>
2021-02-24 12:13:20 +0100

    Fikset formatering av logo

--- components/header.js
+++ components/header.js
@@ -96,7 +96,7 @@ const useStyles = makeStyles((theme) => ({
                 <div>
                 <Link href={'/'}>
                 <a>
-                <Image src='/Logo.png' height={60} width={200} paddingTop='100px'/>
+                <Image src='/Logo.png' height={45} width={197} paddingTop='100px'/>
                 </a>
                 </Link>
                 </div>
