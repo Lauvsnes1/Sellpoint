47ab4e4b438069fee2eedd1a210f25f145ca7148
Andrea Hetlevik Vanebo <andrehva@stud.ntnu.no>
2021-03-11 15:02:43 +0100

    [WIP] reklame oppstart, med Lavrans. #24

---
+++ components/reklame.js
@@ -0,0 +1,12 @@
+export default function Reklame(){
+
+    return(
+        <>
+        <style jsx>{`
+        
+      `}</style>
+        <div className="left"></div>
+        <div className="right"></div>
+        </>
+    );
+}
