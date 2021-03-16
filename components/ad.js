c899954ce30cb1a0e8b4f850d93ef8bd1878276e
Andrea Hetlevik Vanebo <andrehva@stud.ntnu.no>
2021-03-16 14:34:04 +0100

    #24 Lavrans og Andrea. Lagt til reklame som endres

--- components/ad.js
+++ components/ad.js
@@ -1,99 +1,99 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import { colors, ThemeProvider} from '@material-ui/core';
-import { green, blue, yellow, darkGreen, lightGreen }from '@material-ui/core/colors/green'
-import {red}from '@material-ui/core/colors'
-import theme from '../src/theme';
-import Image from 'next/image'
-import Link from 'next/link'
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import fire from '../config/fire-config';
+import React from "react";
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import { colors, ThemeProvider } from "@material-ui/core";
+import {
+  green,
+  blue,
+  yellow,
+  darkGreen,
+  lightGreen,
+} from "@material-ui/core/colors/green";
+import { red } from "@material-ui/core/colors";
+import theme from "../src/theme";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import fire from "../config/fire-config";
 import firebase from "../config/fire-config";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 const useStyles = makeStyles((theme) => ({
+  reklameContainer: {
+    width: 1200,
+    height: 500,
+    display: "flex",
+    alignSelf: "center",
+    paddingLeft: 0,
+    flexDirection: "column",
+    position: "fixed",
+    paddingBottom: 50,
+  },
 
-    reklameContainer: {
-        width: 1200,
-        height: 500,
-        display: "flex",
-        alignSelf: "center",
-        paddingLeft: 0,
-        flexDirection: "column",
-        position: "fixed",
-        paddingBottom: 50
-    },
-
-    reklameContainerLeft: {
-        alignSelf: "flex-start",
-        position: "fixed",
-        top: 50,
-        width: 200,
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        paddingLeft: 0,
-        paddingTop:71
-    },
-    reklameContainerRight: {
-        alignSelf: "flex-end",
-        position: "fixed",
-        top: 50,
-        width: 200,
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "flex-end",
-        paddingRight: 0,
-        paddingTop:71
-    }
+  reklameContainerLeft: {
+    position: "fixed",
+    top: 130,
+    width: 200,
+    left: 10,
+  },
+  reklameContainerRight: {
+    position: "fixed",
+    top: 130,
+    width: 200,
+    right: 10,
+  },
 }));
 
-function Reklame(){ 
-    const [ads, setAd] = useState([])
-
-    useEffect(() => {
-        firebase
-        .firestore()
-        .collection("ads")
-        .onSnapshot((snapShot) => {
-            const newAds = snapShot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-            setAd(newAds);
-        })
-    }, [])
-    return ads
+async function getAds() {
+  const snapshot = await firebase.firestore().collection("ads").get();
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
 }
 
-const AdCards = () => {
-    const ads = Reklame();
-    const classes = useStyles();
+export default function AdCards() {
+  const classes = useStyles();
+  const [ad, setAd] = useState(null);
+  let ads = [];
+  let i = 0;
 
-    return(
-        <div className= {classes.reklameContainer}>
-            <div className= {classes.reklameContainerLeft}>
-                {ads.map((ad) => 
-                    <a href={ad.link}>
-                        <Image src={ad.imageUrl} height={450} width={200}/>
-                    </a>
-                )}
-            </div>
-            <div className= {classes.reklameContainerRight}>
-                {ads.map((ad) => 
-                    <a href={ad.link}>
-                        <Image src={ad.imageUrl} height={450} width={200}/>
-                    </a>
-                )}
-            </div>
+  useEffect(() => {
+    getAds()
+      .then((stuff) => {
+        ads = stuff;
+      })
+      .then(() => {
+        setAd(ads[0]);
+      });
+    setInterval(function () {
+      console.log(i);
+      console.log(ads[i]);
+      setAd(ads[i]);
+      i = (i + 1) % ads.length;
+    }, 10000);
+  }, []);
+
+  if (ad) {
+    return (
+      <div className={classes.reklameContainer}>
+        <div className={classes.reklameContainerLeft}>
+          <a href={ad.link} rel="noopener noreferrer" target="_blank">
+            <Image src={ad.imageUrl} height={575} width={200} />
+          </a>
         </div>
-        
+        <div className={classes.reklameContainerRight}>
+          <a href={ad.link} rel="noopener noreferrer" target="_blank">
+            <Image src={ad.imageUrl} height={575} width={200} />
+          </a>
+        </div>
+      </div>
     );
-};
-
-export default AdCards
+  } else {
+    return <></>;
+  }
+}
