5fcf176464d04c3b5c8ef9c9e33006e12eac10e4
Even Lauvsnes <evenlau@stud.ntnu.no>
2021-03-17 10:03:20 +0100

    formatering med prettier

--- components/cards_alt.jsx
+++ components/cards_alt.jsx
@@ -1,77 +1,67 @@
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import theme from '../src/theme';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import theme from "../src/theme";
 import firebase from "../config/fire-config";
-import React, { useContext, useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import styles from '../styles/Home.module.css'
-import { colors, ThemeProvider} from '@material-ui/core';
-import Link from 'next/link';
-import TextField from '@material-ui/core/TextField';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import styles from "../styles/Home.module.css";
+import { colors, ThemeProvider } from "@material-ui/core";
+import Link from "next/link";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(() => ({
+  subheader: {
+    color: "#90CC00", //får det ikke til med theme.palette???
+  },
 
-    subheader: {
-      color: '#90CC00'//får det ikke til med theme.palette???
-    },
+  root: {
+    minWidth: 330,
+    maxWidth: 330,
+    height: 375,
+    marginTop: 30,
+    alignItems: "flex-start",
+  },
+  media: {
+    height: 100,
+    paddingTop: "56.25%", // 16:9
+  },
 
-    root: {
-      minWidth: 330,
-      maxWidth: 330,
-      height: 375,
-      marginTop: 30,
-      alignItems: 'flex-start',
+  button: {
+    paddingleft: "100px",
+    outlineOffset: "100px",
+  },
 
-      
-    },
-    media: {
-      height: 100,
-      paddingTop: '56.25%', // 16:9
-    },
+  avatar: {
+    //legg inn profilbilde her senere
+    backgroundColor: "secondary",
+  },
+}));
 
-    button: {
-      paddingleft: '100px',
-      outlineOffset: '100px',
-    },
-
-    
- 
-    avatar: {
-        //legg inn profilbilde her senere
-      backgroundColor: 'secondary',
-    },
-  }));
-
-function usePosts(){
-  const [posts, setPost] = useState([])
+function usePosts() {
+  const [posts, setPost] = useState([]);
   useEffect(() => {
     firebase
-    .firestore()
-    .collection('posts')
-    //.where('price', '>', priceContextMin)
-    //.where('price', '<', priceContextMax)
-    .onSnapshot((snapShot) => {
-      
-      const newPosts = snapShot.docs.map((doc) =>({
-        id: doc.id,
-        ...doc.data()
-
-      }) )
-      setPost(newPosts);
-    } )
-  
-  }, [])
-  return posts
+      .firestore()
+      .collection("posts")
+      //.where('price', '>', priceContextMin)
+      //.where('price', '<', priceContextMax)
+      .onSnapshot((snapShot) => {
+        const newPosts = snapShot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPost(newPosts);
+      });
+  }, []);
+  return posts;
 }
-function SortByPrice(props){
+function SortByPrice(props) {
   const classes = useStyles();
   const minValue = props.minValue;
   const maxValue = props.maxValue;
@@ -79,186 +69,235 @@ function SortByPrice(props){
   const searched = props.searched;
   const allPosts = usePosts();
 
-  const [posts, setPost] = useState([])
+  const [posts, setPost] = useState([]);
   useEffect(() => {
-    console.log('minValue:', minValue,'maxValue:', maxValue);
+    console.log("minValue:", minValue, "maxValue:", maxValue);
     firebase
-    .firestore()
-    .collection('posts')
-    .where('price','<=', maxValue)
-    .where('price','>=', minValue)
-    .orderBy('price','asc')
-    .onSnapshot((snapShot) => {
-      
-      const newPosts = snapShot.docs.map((doc) =>({
-        id: doc.id,
-        ...doc.data()
+      .firestore()
+      .collection("posts")
+      .where("price", "<=", maxValue)
+      .where("price", ">=", minValue)
+      .orderBy("price", "asc")
+      .onSnapshot((snapShot) => {
+        const newPosts = snapShot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPost(newPosts);
+      });
+  }, [searchCounter]);
 
-      }) )
-      setPost(newPosts);
-    } )
-  
-  },[searchCounter])
-
-  if(searched){ //dersom 'søk' knappen er trykket
+  if (searched) {
+    //dersom 'søk' knappen er trykket
     return (
       <div className={styles.annonseContainer}>
-      {posts.map((post) => 
-          <Card className={classes.root} key={post.id}>  
+        {posts.map((post) => (
+          <Card className={classes.root} key={post.id}>
             <div>
-        <CardHeader 
-        classes={{
-          subheader: classes.subheader
-        }}
-
-        avatar={
-        <Avatar aria-label="recipe" className={classes.avatar}>
-        {/*firebase.firestore().collection('users').doc(post.userID).get().firstName*/}  
-        </Avatar>
-        } 
-    
-        title = {post.title}
-        subheader={post.place}
-        
-    />
-    <CardMedia
-        image= {post.imageRefs[0].url}
-        className={classes.media}
-        
-    />
-    <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p" style={{height: "25px", lineHeight: "25px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis"}}>
-        {post.miniDescription}
-        </Typography>
-        <Typography style={{textAlign: "right"}}> 
-          {post.price} kr
-
-          
-        </Typography>
-    </CardContent>
-    <CardActions style={{height: "20px", }}>
-      <Button size="small" variant = 'outlined' color='secondary' marginright = "30px" display="inline-block">
-        <Link href={'/annonse/'+post.id} passHref><a>Annonse</a></Link>
-      </Button>
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <Button size="small" variant = 'outlined' color='secondary' marginleft = "30px" display="inline-block">
-        Selger
-      </Button>
-    </CardActions>
-
-  </div>
-    
-  </Card>
-)}
-  </div>
+              <CardHeader
+                classes={{
+                  subheader: classes.subheader,
+                }}
+                avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                    {/*firebase.firestore().collection('users').doc(post.userID).get().firstName*/}
+                  </Avatar>
+                }
+                title={post.title}
+                subheader={post.place}
+              />
+              <CardMedia
+                image={post.imageRefs[0].url}
+                className={classes.media}
+              />
+              <CardContent>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  style={{
+                    height: "25px",
+                    lineHeight: "25px",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  {post.miniDescription}
+                </Typography>
+                <Typography style={{ textAlign: "right" }}>
+                  {post.price} kr
+                </Typography>
+              </CardContent>
+              <CardActions style={{ height: "20px" }}>
+                <Button
+                  size="small"
+                  variant="outlined"
+                  color="secondary"
+                  marginright="30px"
+                  display="inline-block"
+                >
+                  <Link href={"/annonse/" + post.id} passHref>
+                    <a>Annonse</a>
+                  </Link>
+                </Button>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Button
+                  size="small"
+                  variant="outlined"
+                  color="secondary"
+                  marginleft="30px"
+                  display="inline-block"
+                >
+                  Selger
+                </Button>
+              </CardActions>
+            </div>
+          </Card>
+        ))}
+      </div>
     );
-    }
-    else{ //dersom søk-knappen ikke er trykka
-      return (
-        <div className={styles.annonseContainer}>
-        {allPosts.map((post) => 
-            <Card className={classes.root} key={post.id}>  
-              <div>
-          <CardHeader 
-          classes={{
-            subheader: classes.subheader
-          }}
-
-          avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-          {/*firebase.firestore().collection('users').doc(post.userID).get().firstName*/}  
-          </Avatar>
-          
-          } 
-      
-          title = {post.title}
-          subheader={post.place}
-          
-      />
-      {console.log(post.imageRefs[0].url)}
-      <CardMedia
-          className={classes.media}
-          //component='img'
-          image={post.imageRefs[0].url}
-          
-          
-      />
-      
-      <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p" style={{height: "25px", lineHeight: "25px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis"}}>
-          {post.miniDescription}
-          </Typography>
-          <Typography style={{textAlign: "right"}}> 
-            {post.price} kr
-          </Typography>
-      </CardContent>
-      <CardActions style={{height: "20px", }}>
-        <Button size="small" variant = 'outlined' color='secondary' marginright = "30px" display="inline-block">
-          <Link href={'/annonse/'+post.id} passHref><a>Annonse</a></Link>
-        </Button>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <Button size="small" variant = 'outlined' color='secondary' marginleft = "30px" display="inline-block">
-          Selger
-        </Button>
-      </CardActions>
-
-    </div>
-      
-    </Card>
-)}
-    </div>
-      );
-
-    }
- 
-  }
-
-
- const PostCards = () => {
- const [minValue, setMinValue] = useState(0);
- const [maxValue, setMaxValue] = useState(0);
- const [searchCounter, setSearchCounter] = useState(0);
- const classes = useStyles();
- const [postId,setPostId] = useState(0);
- const [searched, setSearched] = useState(false);
-
-  function handlePriceRange(){
-    setSearchCounter(searchCounter+1);
-    setSearched(true); //Varsler for å filtrere kortene
-    setMaxValue(parseInt(maxValue,10)); //OBSOBS parseINT måtte til for å få riktig format på input
-    setMinValue(parseInt(minValue,10));
-  }
-    
+  } else {
+    //dersom søk-knappen ikke er trykka
     return (
+      <div className={styles.annonseContainer}>
+        {allPosts.map((post) => (
+          <Card className={classes.root} key={post.id}>
+            <div>
+              <CardHeader
+                classes={{
+                  subheader: classes.subheader,
+                }}
+                avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                    {/*firebase.firestore().collection('users').doc(post.userID).get().firstName*/}
+                  </Avatar>
+                }
+                title={post.title}
+                subheader={post.place}
+              />
+              {console.log(post.imageRefs[0].url)}
+              <CardMedia
+                className={classes.media}
+                //component='img'
+                image={post.imageRefs[0].url}
+              />
 
-    <ThemeProvider theme ={theme}>
-      {//<PriceRangeSlider getValues={handleSlider}/>
- }
-      <div style={{paddingTop: 100}}>
-        <Typography id="range-slider" gutterBottom >
+              <CardContent>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  style={{
+                    height: "25px",
+                    lineHeight: "25px",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  {post.miniDescription}
+                </Typography>
+                <Typography style={{ textAlign: "right" }}>
+                  {post.price} kr
+                </Typography>
+              </CardContent>
+              <CardActions style={{ height: "20px" }}>
+                <Button
+                  size="small"
+                  variant="outlined"
+                  color="secondary"
+                  marginright="30px"
+                  display="inline-block"
+                >
+                  <Link href={"/annonse/" + post.id} passHref>
+                    <a>Annonse</a>
+                  </Link>
+                </Button>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Button
+                  size="small"
+                  variant="outlined"
+                  color="secondary"
+                  marginleft="30px"
+                  display="inline-block"
+                >
+                  Selger
+                </Button>
+              </CardActions>
+            </div>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+}
+
+const PostCards = () => {
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(0);
+  const [searchCounter, setSearchCounter] = useState(0);
+  const classes = useStyles();
+  const [postId, setPostId] = useState(0);
+  const [searched, setSearched] = useState(false);
+
+  function handlePriceRange() {
+    setSearchCounter(searchCounter + 1);
+    setSearched(true); //Varsler for å filtrere kortene
+    setMaxValue(parseInt(maxValue, 10)); //OBSOBS parseINT måtte til for å få riktig format på input
+    setMinValue(parseInt(minValue, 10));
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      {
+        //<PriceRangeSlider getValues={handleSlider}/>
+      }
+      <div style={{ paddingTop: 100 }}>
+        <Typography id="range-slider" gutterBottom>
           Prisområde:
         </Typography>
-        <div className={classes.textInputContainer} >
-        <TextField value={minValue} id="outlined-basic" label="Min pris" variant="outlined" key={'1'} type='number' onChange={({target}) => setMinValue(target.value)} />
-        <TextField value={maxValue} id="outlined-basic" label="Max pris" variant="outlined" key={'2'} type='number' onChange={({target}) => setMaxValue(target.value)} />
-        <Button variant="contained" 
-        onClick={handlePriceRange}
-        color='secondary'>Søk</Button>
+        <div className={classes.textInputContainer}>
+          <TextField
+            value={minValue}
+            id="outlined-basic"
+            label="Min pris"
+            variant="outlined"
+            key={"1"}
+            type="number"
+            onChange={({ target }) => setMinValue(target.value)}
+          />
+          <TextField
+            value={maxValue}
+            id="outlined-basic"
+            label="Max pris"
+            variant="outlined"
+            key={"2"}
+            type="number"
+            onChange={({ target }) => setMaxValue(target.value)}
+          />
+          <Button
+            variant="contained"
+            onClick={handlePriceRange}
+            color="secondary"
+          >
+            Søk
+          </Button>
         </div>
-        </div>
-        <SortByPrice searched={searched} maxValue={maxValue} minValue={minValue} searchCounter={searchCounter} />
-        <div/>
+      </div>
+      <SortByPrice
+        searched={searched}
+        maxValue={maxValue}
+        minValue={minValue}
+        searchCounter={searchCounter}
+      />
+      <div />
+    </ThemeProvider>
+  );
+};
 
-          </ThemeProvider>
-
-      );
-        };
-    
-        export default PostCards
-
-
-    
+export default PostCards;
