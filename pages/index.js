e780dd9b57b516dd90eb879f9b2e735a16949363
Andrea Hetlevik Vanebo <andrehva@stud.ntnu.no>
2021-03-26 07:20:40 +0100

    #24 lagt til opprett-reklame-knapp for reklamører

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,12 @@ import Card from "../components/cards";
 import PostCards from "../components/cards_alt";
 import fire from "../config/fire-config";
 import { useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Link from "next/link";
 
 export default function Home() {
   const [admin, setAdmin] = useState(false);
+  const [advertiser, setAdvertiser] = useState(false);
   useEffect(() => {
     fire.auth().onAuthStateChanged((user) => {
       if (user) {
@@ -17,7 +20,23 @@ export default function Home() {
           .collection("users")
           .doc(user.uid)
           .get()
-          .then((doc) => setAdmin(doc.data()?.permissions.admin ? true : false));
+          .then((doc) =>
+            setAdmin(doc.data()?.permissions.admin ? true : false)
+          );
+      }
+    });
+  });
+  useEffect(() => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        fire
+          .firestore()
+          .collection("users")
+          .doc(user.uid)
+          .get()
+          .then((doc) =>
+            setAdvertiser(doc.data()?.permissions.advertiser ? true : false)
+          );
       }
     });
   });
@@ -29,6 +48,7 @@ export default function Home() {
       <div className={styles.rad}>
         <div className={styles.annonseContainer}>
           <AdminHeadLine admin={admin} />
+          <AdvertiserHeadLine advertiser={advertiser} />
           <PostCards />
         </div>
       </div>
@@ -41,6 +61,27 @@ function AdminHeadLine(props) {
 
   if (admin) {
     return <h1>Du er logget inn som admin</h1>;
+  } else {
+    return <></>;
+  }
+}
+
+function AdvertiserHeadLine(props) {
+  const advertiser = props.advertiser;
+
+  if (advertiser) {
+    return (
+      <div>
+        <h1>Du er logget inn som reklamør</h1>
+        <Link href="/createAd">
+          <a>
+            <Button color="secondary" variant="contained" type="submit">
+              Opprett reklame
+            </Button>
+          </a>
+        </Link>
+      </div>
+    );
   } else {
     return <></>;
   }
