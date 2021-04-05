79614fd4b5cbed8403aea3390d5fee8f62dc2d71
Andrea Hetlevik Vanebo <andrehva@stud.ntnu.no>
2021-04-05 16:03:50 +0200

    #25 små endringer

--- components/rating.jsx
+++ components/rating.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import fire from "../config/fire-config";
 
 const useStyles = makeStyles({
   root: {
@@ -31,12 +30,10 @@ const RatingCards = ({ ratings }) => {
       {ratings.map((rating) => (
         <Card className={classes.root} variant="outlined">
           <CardContent>
-            <Typography variant="h5" component="h2"></Typography>
             <Typography className={classes.pos}>
               {"Rating score: "+rating.rating}
             </Typography>
-            <Typography variant="body2" component="p">              
-              <br />
+            <Typography variant="body2" component="p">
               {"Beskrivelse: "+rating.reviewText}
             </Typography>
           </CardContent>
